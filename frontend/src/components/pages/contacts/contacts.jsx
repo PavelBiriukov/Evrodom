1bda461f9a57a52c71670bf6d94a17df519da7f1
PavelBiriukov <90255443+PavelBiriukov@users.noreply.github.com>
2024-02-12 20:38:42 +0600

    версия для телефонов

--- frontend/src/components/pages/contacts/contacts.jsx
+++ frontend/src/components/pages/contacts/contacts.jsx
@@ -19,8 +19,16 @@ const Contacts = () => {
                             <div class="page_text">
                                 <p><strong>Мы находимся по адресу:</strong></p>
                                 <p>г. Кара-Балта ул. Жайыл Батыра 249</p>
-                                <p><strong>Наши телефоны: +996 557 23 03 90; +996 501 23 03 90; +996 555 46 28 90.</strong> </p>
-                                <iframe allowfullscreen="" height="450" loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d649.3212371974854!2d73.8471229233014!3d42.8242855537232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389f38794e26b4bf%3A0x9ba61178d7d113d2!2z0JrQsNGE0LUg0JDQu9C10LrRgQ!5e1!3m2!1sru!2skg!4v1698069028843!5m2!1sru!2skg" style={{border:'0', width:"100%"}}></iframe>
+                                <p>
+                                    <strong>Наши телефоны: 
+                                        <ul>
+                                            <li>+996 557 23 03 90</li>
+                                            <li>+996 501 23 03 90</li>
+                                            <li>+996 555 46 28 90</li>
+                                        </ul> 
+                                    </strong> 
+                                </p>
+                                <iframe allowfullscreen="" height="450" loading="lazy" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d649.3212371974854!2d73.8471229233014!3d42.8242855537232!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x389f38794e26b4bf%3A0x9ba61178d7d113d2!2z0JrQsNGE0LUg0JDQu9C10LrRgQ!5e1!3m2!1sru!2skg!4v1698069028843!5m2!1sru!2skg" style={{border:'0', width:"100%",height: '50vh'}}></iframe>
                                 
                             </div>
                         </div>
